fec90a09f7365e412befc80fe9b854170f4b11e1
zakrent <szymon.zakrent@gmail.com>
2017-02-10 18:50:29 +0100

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,32 @@
+from classes import *
+#Debugging
+x = Enigma()
+x.rotors[2].position = 0
+x.rotors[1].position = 0
+x.rotors[0].position = 3
+
+print(x.encrypt(Char('A')))
+print(x.encrypt(Char('B')))
+print(x.encrypt(Char('C')))
+print(x.encrypt(Char('D')))
+print(x.encrypt(Char('E')))
+print(x.encrypt(Char('A')))
+print(x.encrypt(Char('B')))
+print(x.encrypt(Char('C')))
+print(x.encrypt(Char('D')))
+print(x.encrypt(Char('E')))
+print(x.encrypt(Char('A')))
+print(x.encrypt(Char('B')))
+print(x.encrypt(Char('C')))
+print(x.encrypt(Char('D')))
+print(x.encrypt(Char('E')))
+print(x.encrypt(Char('A')))
+print(x.encrypt(Char('B')))
+print(x.encrypt(Char('C')))
+print(x.encrypt(Char('D')))
+print(x.encrypt(Char('E')))
+print(x.encrypt(Char('A')))
+print(x.encrypt(Char('B')))
+print(x.encrypt(Char('C')))
+print(x.encrypt(Char('D')))
+print(x.encrypt(Char('E')))
